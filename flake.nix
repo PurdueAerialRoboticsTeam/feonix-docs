4b6020eafb4c766f9ea51b7a97e56b225dd774de
Aarya Patel <aarya.patel@gmail.com>
2024-10-02 13:37:50 -0400

    i think i fixed it

--- flake.nix
+++ flake.nix
@@ -2,77 +2,72 @@
   description = "Feonix Documentation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      perSystem = { config, pkgs, system, ... }:
-        let
-          python3Packages = (pkgs.python3.override {
-            packageOverrides = pFinal: pPrev: {
-              mkdocs-with-pdf = pFinal.callPackage ./mkdocs-with-pdf.nix { };
-              plantuml-markdown = pPrev.plantuml-markdown.override {
-                plantuml = pkgs.plantuml-c4;
-              };
-            };
-          }).pkgs;
-        in
-        {
-          packages = {
-            default = config.packages.html;
-            html = python3Packages.callPackage ./build.nix { };
-            pdf = config.packages.html.overrideAttrs (old: {
-              name = "feonix-documentation.pdf";
-              ENABLE_PDF_EXPORT = 1;
-              buildPhase = ''
-                mkdocs build
-                cp site/pdf/document.pdf $out
-              '';
-            });
-          };
-
-          apps = {
-            default = config.apps.serve-docs;
-            serve-docs = {
-              type = "app";
-              program = builtins.toString (pkgs.writeShellScript "serve-docs" ''
-                exec ${pkgs.python3}/bin/python3 -m http.server \
-                    --bind 127.0.0.1 \
-                    --directory ${config.packages.html}
-              '');
+  outputs = { self, nixpkgs, pre-commit-hooks, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python3Packages = (pkgs.python3.override {
+          packageOverrides = pFinal: pPrev: {
+            mkdocs-with-pdf = pFinal.callPackage ./mkdocs-with-pdf.nix { };
+            plantuml-markdown = pPrev.plantuml-markdown.override {
+              plantuml = pkgs.plantuml-c4;
             };
           };
-
-          checks = {
-            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                cspell = {
-                  enable = true;
-                  entry = "${pkgs.nodePackages.cspell}/bin/cspell --words-only";
-                  types = [ "markdown" ];
-                };
-                markdownlint.enable = true;
-                nixpkgs-fmt.enable = true;
-                statix.enable = true;
-              };
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.packages.html ];
-            shellHook = ''
-              ${config.checks.pre-commit-check.shellHook}
+        }).pkgs;
+      in
+      rec {
+        packages = {
+          html = python3Packages.callPackage ./build.nix { };
+          pdf = packages.html.overrideAttrs (old: {
+            name = "feonix-documentation.pdf";
+            ENABLE_PDF_EXPORT = "true";
+            buildPhase = ''
+              mkdocs build
+              cp site/pdf/document.pdf $out
             '';
+          });
+          default = packages.html;
+        };
+
+        apps = {
+          serve-docs = {
+            type = "app";
+            program = builtins.toString (pkgs.writeShellScript "serve-docs" ''
+              exec ${pkgs.python3}/bin/python3 -m http.server \
+              --bind 127.0.0.1 \
+              --directory ${packages.html}
+            '');
+          };
+          default = apps.serve-docs;
+        };
+
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              cspell = {
+                enable = true;
+                entry = "${pkgs.nodePackages.cspell}/bin/cspell --words-only";
+                types = [ "markdown" ];
+              };
+              markdownlint.enable = true;
+              nixpkgs-fmt.enable = true;
+              statix.enable = true;
+            };
           };
         };
-    };
+
+        devShell = with pkgs; mkShell {
+          inputsFrom = [ packages.html ];
+          shellHook = ''
+            ${checks.pre-commit-check.shellHook}
+          '';
+        };
+      }
+    );
 }
